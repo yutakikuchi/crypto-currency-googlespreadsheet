3f4e10ffabe0c632ede24fbf4f079c09e9f4bb8d
yutakikuchi <k.yuta@abeja.asia>
2019-05-04 23:04:55 +0900

    change the description

---
+++ currency_table_gen.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+def main():
+
+  if( len(sys.argv) < 2 ) :
+    print('Usage : python currency_table_gen.py 100')
+    sys.exit()
+
+  before_date = int(sys.argv[1])
+  base_import_string = '=IMPORTXML(\'Parameter Sheet\'!$B$1,"//*[@id=\'historical-data\']/div/div[2]/table/tbody/tr[{}]/td[{}]")'
+  base_currency_string = 'INDEX(GoogleFinance("currency:USDJPY", "price", A{}),2,2)'
+
+  print('date', '\t', 'start_price', '\t', 'end_price', '\t', 'min_price', '\t', 'max_price', '\t' , 'ave price', sep='')
+
+  for i in range(1, before_date) :
+    print( base_import_string.format(i, 1), '\t',
+           base_import_string.format(i, 2), ' * ', base_currency_string.format(i + 1), '\t',
+           base_import_string.format(i, 5), ' * ', base_currency_string.format(i + 1), '\t',
+           base_import_string.format(i, 4), ' * ', base_currency_string.format(i + 1), '\t',
+           base_import_string.format(i, 3), ' * ', base_currency_string.format(i + 1), '\t',
+           '=AVERAGE(D{}:E{})'.format(i, i), sep = '')
+
+if __name__ == '__main__':
+  main()
